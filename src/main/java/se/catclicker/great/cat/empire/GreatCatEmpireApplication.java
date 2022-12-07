0eff7e6e9cc359ea155a26d6b9921a902ec6ac2d
Luca A <lucale1114@skola.goteborg.se>
2022-12-07 10:23:33 +0100

    first commit

---
+++ src/main/java/se/catclicker/great/cat/empire/GreatCatEmpireApplication.java
@@ -0,0 +1,13 @@
+package se.catclicker.great.cat.empire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatCatEmpireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatCatEmpireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/catclicker/great/cat/empire/GreatCatEmpireApplicationTests.java
@@ -0,0 +1,13 @@
+package se.catclicker.great.cat.empire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatCatEmpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
