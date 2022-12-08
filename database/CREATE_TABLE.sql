1e696c2dc0fa6253a9979e1624b80193b6ed9982
Luca A <lucale1114@skola.goteborg.se>
2022-12-08 14:59:56 +0100

    changed sql

--- database/CREATE_TABLE.sql
+++ database/CREATE_TABLE.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE catempire;
 USE catempire;
 
-CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(15), pass VARCHAR(100), meowllings INT); 
-INSERT INTO users VALUES ("User", "Pass", 10);
+-- Table is "user"
+INSERT INTO user VALUES ("User", "Pass", 10);
