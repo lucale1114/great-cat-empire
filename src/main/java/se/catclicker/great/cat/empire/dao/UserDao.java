83d0801d45906792c321fada091377f39099e335
Oskar Johansson <oskargj.johansson@gmail.com>
2022-12-13 12:52:36 +0100

    created UserDao, UserId and added some dependency

---
+++ src/main/java/se/catclicker/great/cat/empire/dao/UserDao.java
@@ -0,0 +1,47 @@
+package se.catclicker.great.cat.empire.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.security.auth.login.AccountNotFoundException;
+//import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+
+import se.catclicker.great.cat.empire.forms.UserId;
+
+
+public class UserDao {
+    private static Logger logger = LoggerFactory.getLogger(UserDao.class);
+    private DataSource ds;
+
+    //@InjectService
+    UserDao(DataSource ds) {
+        this.ds = ds;
+    }
+
+    public UserId validate(String user, String password) {
+        try (Connection conn = ds.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(
+                        "SELECT User, Pass FROM user WHERE user = '" + user + "'")) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String passwordHash = rs.getString("password_hash");
+                Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
+                if (encoder.matches(password, passwordHash)) {
+                    return UserId.of(id);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new AccountNotFoundException(ex);   
+        }
+
+        throw new AccountNotFoundException("Unable to find user " + user);
+    }
+}
