b0899e07d49830500ea05d4d8a5c7988701de0b4
Luca A <lucale1114@skola.goteborg.se>
2022-12-15 13:56:11 +0100

    what

--- frontend/js/User.js
+++ frontend/js/User.js
@@ -1,8 +1,9 @@
 export default class User {
-	constructor(id, username, password, meowllings) {
+	constructor(id, username, password, meowllings, catAmount) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.meowllings = meowllings;
+        this.catAmount = catAmount;
     }
 }
